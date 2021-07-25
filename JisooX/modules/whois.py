8fcc9f657d9cbd44c163c7486d2e0a8f3e254cee
riz.ex <86570072+rizkyoye@users.noreply.github.com>
2021-07-26 01:02:39 +0700

    Update whois.py

--- JisooX/modules/whois.py
+++ JisooX/modules/whois.py
@@ -44,20 +44,20 @@ def info(bot: Bot, update: Update, args: List[str]):
         return
     
     text = (f"<b>User Information:</b>\n"
-            f"🆔: <code>{user.id}</code>\n"
-            f"👤Name: {html.escape(user.first_name)}")
+            f"ID: <code>{user.id}</code>\n"
+            f"Nama: {html.escape(user.first_name)}")
 
     if user.last_name:
-        text += f"\n🚹Last Name: {html.escape(user.last_name)}"
+        text += f"\nLast Name: {html.escape(user.last_name)}"
 
     if user.username:
-        text += f"\n♻️Username: @{html.escape(user.username)}"
+        text += f"\nUsername: @{html.escape(user.username)}"
 
-    text += f"\n☣️Permanent user link: {mention_html(user.id, 'link🚪')}"
+    text += f"\nPermanent user link: {mention_html(user.id, 'link🚪')}"
 
     num_chats = sql.get_user_num_chats(user.id)
-    text += f"\n🌐Chat count: <code>{num_chats}</code>"
-    text += "\n🎭Number of profile pics: {}".format(bot.get_user_profile_photos(user.id).total_count)
+    text += f"\nChat count: <code>{num_chats}</code>"
+    text += "\nNumber of profile pics: {}".format(bot.get_user_profile_photos(user.id).total_count)
    
     try:
         user_member = chat.get_member(user.id)
@@ -66,14 +66,14 @@ def info(bot: Bot, update: Update, args: List[str]):
             result = result.json()["result"]
             if "custom_title" in result.keys():
                 custom_title = result['custom_title']
-                text += f"\n🛡This user holds the title⚜️ <b>{custom_title}</b> here."
+                text += f"\nThis user holds the title <b>{custom_title}</b> here."
     except BadRequest:
         pass
 
    
 
     if user.id == OWNER_ID:
-        text += "\n🚶🏻‍♂️Uff,This person is my Owner🤴\nI would never do anything against him!."
+        text += "\nUpss, dia adalah owner ku🌻\nI would never do anything against him!."
         
     elif user.id in DEV_USERS:
         text += "\n🚴‍♂️Pling,This person is my dev🤷‍♂️\nI would never do anything against him!."
@@ -95,7 +95,7 @@ def info(bot: Bot, update: Update, args: List[str]):
         text += "\n🚴‍♂️Pling,This person has been whitelisted! " \
                         "That means I'm not allowed to ban/kick them."
     elif user.id == bot.id:     
-        text += "\n💃Lol🧞‍♂️It's Me😉"
+        text += "\nlol, ini aku sendiri🌻"
 
 
     text +="\n"
