ffab3728492e7d326044d8fd9fea4a67505c3c80
riz.ex <86570072+rizkyoye@users.noreply.github.com>
2021-07-28 19:30:29 +0700

    Update misc.py

--- JisooX/modules/helper_funcs/misc.py
+++ JisooX/modules/helper_funcs/misc.py
@@ -68,9 +68,9 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 7:
         pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("🔻", callback_data="{}_prev({})".format(prefix, modulo_page)),
+            (EqInlineKeyboardButton("🔺", callback_data="{}_prev({})".format(prefix, modulo_page)),
                 EqInlineKeyboardButton("[ ʙᴀᴄᴋ ]", callback_data="bot_start"),
-             EqInlineKeyboardButton("🔺", callback_data="{}_next({})".format(prefix, modulo_page)))]
+             EqInlineKeyboardButton("🔻", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
         pairs += [[EqInlineKeyboardButton("[ ʙᴀᴄᴋ ]", callback_data="bot_start")]]
