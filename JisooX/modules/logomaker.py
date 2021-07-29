22d241d65e6749e28d8c3e8ac7df1ed8bdc13615
riz.ex <86570072+rizkyoye@users.noreply.github.com>
2021-07-29 14:33:14 +0700

    Update logomaker.py

--- JisooX/modules/logomaker.py
+++ JisooX/modules/logomaker.py
@@ -69,86 +69,105 @@ TELEGRAPH_MEDIA_LINKS = ["https://telegra.ph/file/e354ce72d5cc6a1d27c4d.jpg",
                          "https://telegra.ph/file/39f7b5d1d7a3487f6ba69.jpg"
                          ]
 
-@register(pattern="^/logo ?(.*)")
-async def lego(event):
- quew = event.pattern_match.group(1)
- if event.sender_id == OWNER_ID:
-     pass
- else:
-     
-    if not quew:
-       await event.reply('Provide Some Text To Draw!')
-       return
+Commands Available -
+
+• `{i}logo <text>`
+   Generate a logo of the given Text
+   Or Reply To image , to write ur text on it.
+   Or Reply To Font File, To write with that font.
+
+"""
+import glob
+import os
+import random
+
+from PIL import Image, ImageDraw, ImageFont
+from telethon.tl.types import InputMessagesFilterPhotos
+
+from . import *
+
+
+@ultroid_cmd(pattern="logo ?(.*)")
+async def logo_gen(event):
+    xx = await eor(event, get_string("com_1"))
+    name = event.pattern_match.group(1)
+    if not name:
+        await eod(xx, "`Give a name too!`")
+    bg_, font_ = "", ""
+    if event.reply_to_msg_id:
+        temp = await event.get_reply_message()
+        if temp.media:
+            if hasattr(temp.media, "document"):
+                if "font" in temp.file.mime_type:
+                    font_ = await temp.download_media()
+                elif (".ttf" in temp.file.name) or (".otf" in temp.file.name):
+                    font_ = await temp.download_media()
+            elif "pic" in mediainfo(temp.media):
+                bg_ = await temp.download_media()
     else:
-       pass
- await event.reply('Creating your logo...wait!')
- try:
-    text = event.pattern_match.group(1)
-    img = Image.open('./AlinaEx/resources/blackbg.jpg')
-    draw = ImageDraw.Draw(img)
-    image_widthz, image_heightz = img.size
-    pointsize = 500
-    fillcolor = "gold"
-    shadowcolor = "blue"
-    font = ImageFont.truetype("./AlinaEx/resources/Chopsic.otf", 330)
-    w, h = draw.textsize(text, font=font)
-    h += int(h*0.21)
-    image_width, image_height = img.size
-    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
-    x = (image_widthz-w)/2
-    y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
-    fname2 = "LogoByAlina.png"
-    img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @alinaexbot Join @nyanyiisinii")
-    if os.path.exists(fname2):
-            os.remove(fname2)
- except Exception as e:
-   await event.reply(f'Error!, {e}')
-
-
-
-   
-@register(pattern="^/wlogo ?(.*)")
-async def lego(event):
- quew = event.pattern_match.group(1)
- if event.sender_id == OWNER_ID:
-     pass
- else:
-     
-    if not quew:
-       await event.reply('Provide Some Text To Draw!')
-       return
+        pics = []
+        async for i in event.client.iter_messages(
+            "@UltroidLogos", filter=InputMessagesFilterPhotos
+        ):
+            pics.append(i)
+        id_ = random.choice(pics)
+        bg_ = await id_.download_media()
+        fpath_ = glob.glob("resources/fonts/*")
+        font_ = random.choice(fpath_)
+    if not bg_:
+        pics = []
+        async for i in event.client.iter_messages(
+            "@UltroidLogos", filter=InputMessagesFilterPhotos
+        ):
+            pics.append(i)
+        id_ = random.choice(pics)
+        bg_ = await id_.download_media()
+    if not font_:
+        fpath_ = glob.glob("resources/fonts/*")
+        font_ = random.choice(fpath_)
+    if len(name) <= 8:
+        fnt_size = 150
+        strke = 10
+    elif len(name) >= 9:
+        fnt_size = 50
+        strke = 5
     else:
-       pass
- await event.reply('Creating your logo...wait!')
- try:
-    text = event.pattern_match.group(1)
-    img = Image.open('./AlinaEx/resources/blackbg.jpg')
+        fnt_size = 130
+        strke = 20
+    img = Image.open(bg_)
     draw = ImageDraw.Draw(img)
-    image_widthz, image_heightz = img.size
-    pointsize = 500
-    fillcolor = "white"
-    shadowcolor = "blue"
-    font = ImageFont.truetype("./AlinaEx/resources/Maghrib.ttf", 1000)
-    w, h = draw.textsize(text, font=font)
-    h += int(h*0.21)
+    font = ImageFont.truetype(font_, fnt_size)
+    w, h = draw.textsize(name, font=font)
+    h += int(h * 0.21)
     image_width, image_height = img.size
-    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
-    x = (image_widthz-w)/2
-    y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
-    fname2 = "LogoByAlina.png"
-    img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @alinaexbot")
-    if os.path.exists(fname2):
-            os.remove(fname2)
- except Exception as e:
-   await event.reply(f'Error!, {e}')
-
-file_help = os.path.basename(__file__)
-file_help = file_help.replace(".py", "")
-file_helpo = file_help.replace("_", " ")
+    draw.text(
+        ((image_width - w) / 2, (image_height - h) / 2),
+        name,
+        font=font,
+        fill=(255, 255, 255),
+    )
+    x = (image_width - w) / 2
+    y = (image_height - h) / 2
+    draw.text(
+        (x, y), name, font=font, fill="white", stroke_width=strke, stroke_fill="black"
+    )
+    flnme = f"ultd.png"
+    img.save(flnme, "png")
+    await xx.edit("`Done!`")
+    if os.path.exists(flnme):
+        await event.client.send_file(
+            event.chat_id,
+            file=flnme,
+            caption=f"Logo by [{OWNER_NAME}](tg://user?id={OWNER_ID})",
+            force_document=True,
+        )
+        os.remove(flnme)
+        await xx.delete()
+    if os.path.exists(bg_):
+        os.remove(bg_)
+    if os.path.exists(font_):
+        if not font_.startswith("resources/fonts"):
+            os.remove(font_)
 
 
 __help__ = """
